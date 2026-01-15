4153980adf498cd605d0cd6f7b25561b816daea6
Andrew Johnson <andrewjoel88@gmail.com>
2026-01-15 11:46:02 -0800

    tried to figure out bezier shape

--- sketch.js
+++ sketch.js
@@ -1,28 +1,29 @@
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(400,400);
 }
 
 function draw() {
   background(255, 0, 0);
   beginShape();
   vertex(28, 100);
-  quadraticVertex(243, 372, 156, 139);
+  quadraticVertex(243, 372, mouseX, mouseY);
+  console.log("mouseX: "+mouseX, "mouseY: "+mouseY);
   bezierVertex(302, 383, 137, 175, 213, 340);
 endShape(CLOSE)
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
-}
+// //function windowResized(){
+//   //resizeCanvas(windowWidth, windowHeight);
+// }
 
-function myTriangle(amountOfWindow, x, y){
-  push()
-  const size = width * amountOfWindow;
-  //triangleMode(CENTER);
-  translate(x,y);
-  triangle(0, 0, size, size*2 -size, size*2);
-  pop();
-}
+// function myTriangle(amountOfWindow, x, y){
+//   push()
+//   const size = width * amountOfWindow;
+//   //triangleMode(CENTER);
+//   translate(x,y);
+//   triangle(0, 0, size, size*2 -size, size*2);
+//   pop();
+// }
 
 
 
