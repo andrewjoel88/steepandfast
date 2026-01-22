a8f4b0d6557b6520548eb9f730184f0ede0a9da5
Andrew Johnson <andrewjoel88@gmail.com>
2026-01-22 11:50:33 -0800

    changed colors and done

--- sketch.js
+++ sketch.js
@@ -7,18 +7,18 @@ function setup() {
 //
 
 function draw() {
-  background(255, 0, 0);
+  background(132, 177, 186);
 
 
 beginShape();
-fill(0, 255, 0);
+fill(199, 96, 28);
   vertex(15, 17);
   bezierVertex(511, 362, 142, 138, 208, 322);
 
 endShape(CLOSE)
 
 beginShape();
-  fill(255, 0, 0);
+  fill(132, 177, 186);
   noStroke();
   vertex(15, 17);
   quadraticVertex(238, 317, 153, 108);
@@ -26,7 +26,7 @@ endShape(CLOSE)
 
 ellipseMode(CORNER);
 const size = width * 0.3;
-fill(0, 0, 255);
+fill(49, 35, 117);
 ellipse(15, 350, 400, 50);
 
 console.log("mouseX: "+ mouseX+" mouseY: "+ mouseY)
