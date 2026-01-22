5ef0ad8be29e76483ab055bd679f6fc514791428
Andrew Johnson <andrewjoel88@gmail.com>
2026-01-22 10:42:27 -0800

    swapped which shape came first

--- sketch.js
+++ sketch.js
@@ -11,18 +11,22 @@ function draw() {
 //   bezierVertex(302, 383, 137, 175, 213, 340);
 // endShape(CLOSE)
 // }
+
+
+
+
+beginShape();
+  vertex(15, 17);
+  bezierVertex(400, 350, 120, 100, 175, 300);
+endShape(CLOSE)
+
 beginShape();
   vertex(15, 17);
   quadraticVertex(238, 317, 153, 108);
   //bezierVertex(333, 331, 142, 138, 190, 301);
 endShape(CLOSE)
 
-beginShape();
-  vertex(15, 17);
-  bezierVertex(333, 331, 142, 138, 190, 301);
-endShape(CLOSE)
-
-console.log("mouseX: "+ mouseX+" mouseY: "+ mouseY);
+console.log("mouseX: "+ mouseX+" mouseY: "+ mouseY)
 //function windowResized(){
   //resizeCanvas(windowWidth, windowHeight);
 }
